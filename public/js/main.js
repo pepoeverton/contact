878717363fe1cc6c789ea75ba14f5c96bc5da11b
Everton Nunes <everton.code@gmail.com>
2016-02-28 09:30:14 -0300

    configuracao da rotas

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
-angular.module('contact', []);
+angular.module('contact', ['ngRoute'])
+  .config(function($routeProvider){
+
+    $routeProvider.when('/contatos',{
+        templateUrl: 'partials/contatos.html',
+        controller: 'ContactsController'
+    });
+    $routeProvider.when('/contato/:contactId',{
+        templateUrl: 'partials/contato.html',
+        controller: 'ContactController'
+    });
+    $routeProvider.otherwise({
+        redirectTo: '/contatos'
+    });
+  });
