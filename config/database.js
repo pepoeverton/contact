43d6573951fcd1edbd977c4f4fe9074c518bca97
Everton Nunes <everton.code@gmail.com>
2016-03-15 00:16:58 -0300

    modo debug

--- config/database.js
+++ config/database.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose');
 module.exports = function(uri) {
   mongoose.connect(uri);
 
+  mongoose.set('debug',true);
+
   mongoose.connection.on('connected', function(){
     console.log('MongoDB, Conectado em ' + uri);
   });
