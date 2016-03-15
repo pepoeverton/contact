b18315f28b8a6ba3bea5c8a6d80a939002c6ff09
Everton Nunes <everton.code@gmail.com>
2016-03-14 23:35:12 -0300

    conexao com o mongoDB

---
+++ config/database.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+module.exports = function(uri) {
+  mongoose.connect(uri);
+
+  mongoose.connection.on('connected', function(){
+    console.log('MongoDB, Conectado em ' + uri);
+  });
+  mongoose.connection.on('disconnected', function(){
+    console.log('MongoDB, Disonectado em ' + uri);
+  });
+  mongoose.connection.on('error', function(){
+    console.log('MongoDB, Erro na conexão em ' + uri);
+  });
+
+  process.on('SIGINT', function(){
+    mongoose.connection.close(function(){
+      console.log('Término da aplicação!');
+      process.exit(0);
+    });
+  });
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var app = require('./config/express')();
+require('./config/database.js')('mongodb://127.0.0.1/contact');
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express Server escutando na porta ' + app.get('port'));
