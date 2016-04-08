31a04587e729b1d11b717d27d2e3e57b5b9a7471
Everton Nunes <everton.code@gmail.com>
2016-04-08 00:10:13 -0300

    evitando injecao atravez de query selectors

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -2,6 +2,7 @@ module.exports = function(app){
 
   var Contato = app.models.contato;
   var controller = {};
+  var sanitize = require('mongo-sanitize');
 
   controller.listContatos = function(req, res){
     Contato.find().populate('emergencia').exec()
@@ -30,7 +31,7 @@ module.exports = function(app){
       );
   };
   controller.removeContato = function(req, res){
-    var _id = req.params.id;
+    var _id = sanitize(req.params.id);
     Contato.remove({"_id" : _id}).exec()
       .then(
         function(){
@@ -43,11 +44,14 @@ module.exports = function(app){
   };
   controller.salvaContato = function(req, res){
     var _id = req.body._id;
-
-    req.body.emergencia = (req.body.emergencia || null);
+    var dados = {
+      'nome': req.body.nome,
+      'email': req.body.email,
+      'emergencia': req.body.emergencia || null
+    };
 
     if(_id){
-      Contato.findByIdAndUpdate(_id, req.body).exec()
+      Contato.findByIdAndUpdate(_id, dados).exec()
         .then(
           function(contato){
             res.json(contato);
@@ -58,7 +62,7 @@ module.exports = function(app){
           }
         );
     }else{
-      Contato.create(req.body)
+      Contato.create(dados)
         .then(
           function(contato){
             res.json(contato);
